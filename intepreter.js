a7bd74bf9a213b05fafc73b71ece1819191764d3
J R M <jwrmmine@protonmail.com>
2025-06-21 12:47:48 +0200

    Create intepreter.js

---
+++ intepreter.js
@@ -0,0 +1,10 @@
+/*
+* This is the intepreter for the Mallard programming language
+*/
+/*
+* Interpretes Mallard code.
+* @param {String} code - the Mallard code
+* @returns {undefined|String} undefined on success, otherwise error
+function interpret(code){
+  
+}
