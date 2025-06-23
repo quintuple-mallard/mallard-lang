fcb98156cf80411bb4dcdd4a5295b0d98732305f
J R M <jwrmmine@protonmail.com>
2025-06-23 12:42:43 +0200

    Update and rename intepreter.js to compiler.py

---
+++ compiler.py
@@ -0,0 +1,43 @@
+def compileLine(line):
+    return line
+def compile(code):
+    codeDict = {}
+    python = ""
+    lines = code.split("\n")
+    for line in lines:
+        id, statement, goto = line.split("|")
+        id, statement, goto = [item.strip() for item in [id, statement, goto]]
+        if id == "":
+            raise ValueError("Line id must not be blank")
+        if id in codeDict.keys():
+            raise ValueError(f"Line id {id}")
+        codeDict.update(
+            {
+                id:{
+                    "code":statement,
+                    "goto":goto
+                }
+            }
+        )
+    if "init" in codeDict.keys():
+        currentLine = codeDict["init"]
+    else:
+        raise ValueError('The line with id "init" could not be found.')
+    
+    while True:
+        python+=compileLine(currentLine["code"])
+        if currentLine["code"]:
+            python+="\n"
+        if currentLine["goto"]=="":
+            break
+        if currentLine["goto"] in codeDict.keys():
+            currentLine = codeDict[currentLine["goto"]]
+        else:
+            raise ValueError(f'The line with id "{currentLine["goto"]}" could not be found.')
+    return python
+mallard =  """
+init||greet
+greet|Hello,|location
+location|World!|
+""".strip()
+print(compile(mallard))
